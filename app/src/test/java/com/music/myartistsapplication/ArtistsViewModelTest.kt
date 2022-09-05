aeeed0c286a9041ddd5dd425ef7121985375749c
hamzafarooqi <muhamzafarooqi9211@gmail.com>
2022-09-05 06:05:06 +0500

    Added CI and tests

--- app/src/test/java/com/music/myartistsapplication/ArtistsViewModelTest.kt
+++ app/src/test/java/com/music/myartistsapplication/ArtistsViewModelTest.kt
@@ -1,11 +1,51 @@
 package com.music.myartistsapplication
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.music.myartistsapplication.features.artists.artistslisting.ArtistsViewModel
+import com.music.myartistsapplication.repositories.ArtistsRepository
+import com.music.myartistsapplication.usecases.FetchUserUseCase
+import com.music.myartistsapplication.usecases.SearchArtistsUseCase
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
 
 @RunWith(AndroidJUnit4::class)
 class ArtistsViewModelTest {
 
+    @Mock
+    lateinit var artistsRepository : ArtistsRepository
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var artistsViewModel: ArtistsViewModel
+
+    @Mock
+    lateinit var fetchArtistsUseCase : FetchUserUseCase
+
+    @Mock
+    lateinit var searchArtistsUseCase : SearchArtistsUseCase
+
+    @Before
+    fun setUp() {
+        artistsRepository = Mockito.mock(ArtistsRepository::class.java)
+        MockitoAnnotations.initMocks(this)
+        artistsViewModel = ArtistsViewModel(fetchArtistsUseCase,searchArtistsUseCase)
+    }
+
+    @Test
+    suspend fun check_null()
+    {
+        Mockito.`when`(fetchArtistsUseCase.invoke("as",false)).thenReturn(null)
+        Assert.assertNotNull(artistsViewModel.artists)
+    }
 
 }
